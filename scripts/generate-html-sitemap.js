060f073c2eaebf3dbd7ffd34acbe5dd4e1e14daf
startknowledge <82749SONDEEP@GMAIL.COM>
2026-03-16 17:24:03 +0530

    Refactor generate-html-sitemap.js to use ES6 imports

--- scripts/generate-html-sitemap.js
+++ scripts/generate-html-sitemap.js
@@ -1,28 +1,68 @@
-const fs = require("fs")
+import fs from "fs"
 
-let files=fs.readdirSync("pages")
+const pagesDir = "pages"
+const blogDir = "blog"
 
-let html=`
+let html = `
 <!DOCTYPE html>
 <html>
 <head>
 <title>HTML Sitemap</title>
+<meta charset="UTF-8">
 </head>
 
 <body>
 
 <h1>HTML Sitemap</h1>
+
+<h2>Pages</h2>
 <ul>
 `
 
-files.forEach(f=>{
-html+=`<li><a href="/pages/${f}">${f}</a></li>`
+// pages
+if (fs.existsSync(pagesDir)) {
+
+const pages = fs.readdirSync(pagesDir)
+
+pages.forEach(page => {
+
+if(page.endsWith(".html")){
+html += `<li><a href="/pages/${page}">${page.replace(".html","")}</a></li>`
+}
+
 })
 
-html+=`
+}
+
+html += `
 </ul>
+
+<h2>Blog</h2>
+<ul>
+`
+
+// blog
+if (fs.existsSync(blogDir)) {
+
+const blogs = fs.readdirSync(blogDir)
+
+blogs.forEach(blog => {
+
+if(blog.endsWith(".html")){
+html += `<li><a href="/blog/${blog}">${blog.replace(".html","")}</a></li>`
+}
+
+})
+
+}
+
+html += `
+</ul>
+
 </body>
 </html>
 `
 
-fs.writeFileSync("sitemap.html",html)
+fs.writeFileSync("sitemap.html", html)
+
+console.log("HTML sitemap generated")
